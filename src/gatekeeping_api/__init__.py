63f19a464ae8dc266a4cfe4c8263fdaed1d43a94
Kalyan <kalyanrambhukya54@gmail.com>
2026-09-02 12:15:00 +0530

    first commit

---
+++ src/gatekeeping_api/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    JWTManager,
+)
+
+app = Flask(__name__)
+
+database = [
+    {"email": "Kalyan@gmail.com", "password": 123456789},
+    {"email": "Kalyan2@gmail.com", "password": 1234567890},
+]
+
+
+app.config["JWT_SECRET_KEY"] = "kalyan ram"
+jwt = JWTManager(app)
+
+
+@app.post("/login")
+def handle_login():
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    items = list(
+        filter(
+            lambda x: x["email"] == email and x["password"] == int(password), database
+        )
+    )
+    if items:
+        access_token = create_access_token(identity=email)
+        return {
+            "data": "some important data",
+            "access-token": access_token,
+            "success": True,
+            "status": 200,
+        }
+    else:
+        return {"data": "Invalid credentials", "success": False, "status": 400}
+
+
+@app.get("/safe")
+@jwt_required()
+def handle_safe():
+    email = get_jwt_identity()
+    return {"email": email}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
